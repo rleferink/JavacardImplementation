09f7885f55e612b6b4d471d91eb92d540e02dc74
RomyStahli <romystahli@hotmail.com>
2021-04-25 11:33:05 +0200

    Added AES256

--- LoyaltyTerminal/src/PersonalizationTerminal.java
+++ LoyaltyTerminal/src/PersonalizationTerminal.java
@@ -1,5 +1,14 @@
 public class PersonalizationTerminal {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        String originalString = "EncryptionTest";
+
+        String encryptedString = AES256.encrypt(originalString);
+        String decryptedString = AES256.decrypt(encryptedString);
+
+        System.out.println(originalString);
+        System.out.println(encryptedString);
+        System.out.println(decryptedString);
     }
 }
